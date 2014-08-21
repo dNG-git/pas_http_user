02fc03bdd608bb01f673674995be4870f6e29ec9
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-21 20:50:54 +0200

    Trivial: Removed file extension from URLs in the file header

--- src/dNG/pas/module/controller/user/module.py
+++ src/dNG/pas/module/controller/user/module.py
@@ -6,13 +6,13 @@ direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?pas;http;user
+https://www.direct-netware.de/redirect?pas;http;user
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasHttpUserVersion)#
 #echo(__FILEPATH__)#
@@ -33,7 +33,7 @@ Module for "user"
 :package:    pas.http
 :subpackage: user
 :since:      v0.1.00
-:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
              Mozilla Public License, v. 2.0
 	"""
 
